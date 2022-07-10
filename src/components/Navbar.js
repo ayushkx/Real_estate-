94f5dfdea9f08b5963555c75f3451248a2fba46a
ayushkx <ayushkushwaha855@gmail.com>
2022-07-11 00:39:36 +0530

    add data

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,33 +4,33 @@ import HouseIcon from '@mui/icons-material/House';
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/"><HouseIcon/> Dream Home</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg" style={{backgroundColor:'#faddfb'}}>
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/"><span width="30" height="24" className="d-inline-block align-text-top"><HouseIcon/></span> Dream Home</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
                         </li>
-                        <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Dropdown
                         </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"/></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><hr className="dropdown-divider"/></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
                         </ul>
                         </li>
-                        <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
+                        <li className="nav-item">
+                        <a className="nav-link disabled">Disabled</a>
                         </li>
                     </ul>
                     </div>
---
+++ src/components/data.js
@@ -0,0 +1,180 @@
+import KingBedIcon from '@mui/icons-material/KingBed';
+import BathtubIcon from '@mui/icons-material/Bathtub';
+import Crop54Icon from '@mui/icons-material/Crop54';
+
+const data = [
+    {
+        id : 1 ,
+        'name' : 'Corner House' ,
+        'price' : '15000' ,
+        'type' : 'house' ,
+        'date' : 'Today' ,
+        'image' : '../assets/img1.jpg' ,
+        'location':'Delhi',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 2 ,
+        'name' : 'Westerhill' ,
+        'price' : '25000' ,
+        'type' : 'Penthouse' ,
+        'date' : 'August' ,
+        'image' : '../assets/img2.jpg' ,
+        'location':'Mumbai',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 3 ,
+        'name' : 'Crumpsbank House' ,
+        'price' : '35000' ,
+        'type' : 'Farmhouse' ,
+        'date' : 'Today' ,
+        'image' : '../assets/img3.jpg' ,
+        'location':'Noida',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 4,
+        'name' : 'Inglewood' ,
+        'price' : '15000' ,
+        'type' : 'Apartments' ,
+        'date' : 'September' ,
+        'image' : '../assets/img4.jpg' ,
+        'location':'Lucknow',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 5 ,
+        'name' : 'Fox Corner' ,
+        'price' : '20000' ,
+        'type' : 'Farmhouse' ,
+        'date' : 'Today' ,
+        'image' : '../assets/img5.jpg' ,
+        'location':'Delhi',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 6 ,
+        'name' : 'The Crofties' ,
+        'price' : '30000' ,
+        'type' : 'Penthouse' ,
+        'date' : 'September' ,
+        'image' : '../assets/img6.jpg' ,
+        'location':'Delhi',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 7 ,
+        'name' : 'Larkworthy' ,
+        'price' : '25000' ,
+        'type' : 'house' ,
+        'date' : 'August' ,
+        'image' : '../assets/img7.jpg' ,
+        'location':'Noida',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 8 ,
+        'name' : 'Nirvana' ,
+        'price' : '35000' ,
+        'type' : 'Apartments' ,
+        'date' : 'Today' ,
+        'image' : '../assets/img8.jpg' ,
+        'location':'Mumbai',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 9 ,
+        'name' : 'Geeta Bhawan' ,
+        'price' : '25000' ,
+        'type' : 'Farmhouse' ,
+        'date' : 'September' ,
+        'image' : '../assets/img9.jpg' ,
+        'location':'Lucknow',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 10 ,
+        'name' : 'Shanti kunj' ,
+        'price' : '20000' ,
+        'type' : 'Apartments' ,
+        'date' : 'August' ,
+        'image' : '../assets/img10.jpg' ,
+        'location':'Lucknow',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 11 ,
+        'name' : 'Ram Bhawan' ,
+        'price' : '15000' ,
+        'type' : 'house' ,
+        'date' : 'September' ,
+        'image' : '../assets/img11.jpg' ,
+        'location':'Mumbai',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 12,
+        'name' : 'Tythe Barn' ,
+        'price' : '35000' ,
+        'type' : 'Penthouse' ,
+        'date' : 'August' ,
+        'image' : '../assets/img12.jpg' ,
+        'location':'Noida',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 13 ,
+        'name' : 'Lodge' ,
+        'price' : '25000' ,
+        'type' : 'house' ,
+        'date' : 'Today' ,
+        'image' : '../assets/img13.jpg' ,
+        'location':'Delhi',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 14 ,
+        'name' : 'Millfield' ,
+        'price' : '20000' ,
+        'type' : 'house' ,
+        'date' : 'Today' ,
+        'image' : '../assets/img14.jpg' ,
+        'location':'Mumbai',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+    {
+        id : 15 ,
+        'name' : 'Birch Cottage' ,
+        'price' : '35000' ,
+        'type' : 'Farmhouse' ,
+        'date' : 'September' ,
+        'image' : '../assets/img15.jpg' ,
+        'location':'Lucknow',
+        'Address' : '' ,
+        'description' : ''
+    } ,
+
+]
+
+
+
+
+
+			
+			
+	 	
